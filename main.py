cfa25247c6655de5768351252c0f8269a7fb5a37
dennisdinwiddie@gmail.com <dennisdinwiddie@gmail.com>
2019-02-24 23:36:52 -0500

    as;fjalfj;aslkfj;asdlk

--- main.py
+++ main.py
@@ -1,18 +1,121 @@
+import random
+import re
+# read words from a file
 main_text = open('words.txt', "r").read().lower()
 main_text = main_text.split()
 user_name = input("What is your name? ")
 
-comp_word = 'maximize'
-user_guess = ''
-prev_guess = []
-your_guess = input("Enter one letter for a guess: ")
+print("Hello, " + user_name + " are you ready to play hang- Mystery Word!")
+
+difficulty = input("What difficulty would you like to play on? 'Select:\n Easy, \n Medium, \n or Hard \n")
+difficulty = difficulty.casefold()
+easy_list = []
+
+medium_list = []
+
+hard_list = []
+
+# filters words of a certain size into a list based on the difficulty
+def make_diff_list(main_text):
+    for word in main_text:
+        if len(word) in range(4, 7):
+            easy_list.append(word)
+        if len(word) in range(6, 9):
+            medium_list.append(word)
+        if len(word) >= 8:
+            hard_list.append(word)
+    return easy_list, medium_list, hard_list
+easy_list, medium_list, hard_list = make_diff_list(main_text)
+#     computer_list = []
+
+# playing = input("If you would like to play again, enter 'y' ").casefold()d
+while difficulty == 'easy' or 'medium' or 'hard':
+    if difficulty == 'easy':
+        computer_list = easy_list
+        print("\n \nStarting on Easy, you aren't even trying.\n \n")
+        break
+    elif difficulty == 'medium':
+        computer_list = medium_list
+        print("\n \nStarting on Medium, did you go to school for this?\n \n")
+        break
+    elif difficulty == 'hard':
+        computer_list = hard_list
+        print("\n \nStarting on Hard, get ready to get spanked, nerd.\n \n")
+        break
+    else:
+        difficulty = input('Please enter the correct difficulty selector: ')
+        continue
+
+computer_word = None
+# for letter in computer_word:
+# generate a random item from a list, detects the difficulty and then picks a word based on that difficulties word list
+while difficulty == 'easy' or 'medium' or 'hard':
+    if difficulty == 'easy':
+        computer_word = random.choice(easy_list)
+        print("The mystery word is " + str(len(computer_word)) + " characters long.")
+        break
+    elif difficulty == 'medium':
+        computer_word = random.choice(medium_list)
+        print("The mystery word is " + str(len(computer_word)) + " characters long.")
+        break
+    elif difficulty == 'hard':
+        computer_word = random.choice(hard_list)
+        print("The mystery word is " + str(len(computer_word)) + " characters long.")
+        break
+
+# this displays the hidden version of the computer word and checks if the guessed letter is in the computers word and stores it to previous guesses
+mystery_word = re.sub('[a-zA-Z]', '_', computer_word)
+
+def user_guess_func(letter, comp_word, mystery):
+    if_found = False
+    if letter in comp_word:
+        if_found = True
+        for i in range(0, len(comp_word)):
+            if comp_word[i] == letter:
+                mystery = mystery[0:i] + letter + mystery[i+1:len(mystery)]
+    return (if_found, mystery)
+
+print(mystery_word)
 
 
-def checks_word(user_guess, comp_word, prev_guesses):
-    for letter in comp_word:
-        if user_guess in comp_word:
-            prev_guess.append(user_guess)
-        
+def continue_loop():
+    play_again = input('Would you like to play another match? \n Enter: y/n or yes/no  \n' ).lower()
+    if play_again.isalpha() and len(play_again) >= 3 and play_again ==  'y' or 'yes':
+        print("CAN YOU SMELL WHAT THE ROCK, IS, COOKINGGGG????")
+        return True
+    elif play_again.isalpha() and len(play_again) >= 2 and play_again !=  'n' or 'no':
+        print("Come again, scrub.")
+        return False
 
-
-checks_word(your_guess, comp_word, prev_guess)
+play_again = True
+# main gameplay loop and win/lose conditions found here
+turns = 8
+while play_again == True:
+    playing = True
+    while playing == True:
+        while turns > 0:
+            user_guess = input("Enter your guess: ")[:1].lower()
+            if len(user_guess) > 1 or user_guess.isdigit():
+                user_guess = input("Enter a single letter please: ")[:1].lower()
+            letter_found, mystery_word = user_guess_func(user_guess, computer_word, mystery_word)
+            if not letter_found:
+                turns -= 1
+                print("\n A swing and a miss! You have " + str(turns) +" guesses left" "\n " + mystery_word)
+            elif letter_found:
+                print("\n You got one, it will be your last. \n You have " + str(turns) + " guesses left.")
+                print(mystery_word)
+            if "_" not in mystery_word:
+                print('\n' + computer_word + "\n Surprising. Time to up the difficulty, you won. \n You had " + str(turns) + " remaining.")
+                play_again = continue_loop()
+                playing = False
+            if turns == 0:
+                print("\n That's the game! The word was " + computer_word + ". See ya next time. ")
+                play_again = continue_loop()
+                playing = False
+        if 
+    if play_again == True:
+        continue
+    else:
+        print("Later nerd!")
+        break
+    
--- mystery_word.py
+++ mystery_word.py
@@ -1,145 +1,145 @@
-import random
-# Let the user choose a level of difficulty at the beginning of the program. Easy mode only has words of 4-6 characters; normal mode only has words of 6-8 characters; hard mode only has words of 8+ characters.
+# import random
+# # Let the user choose a level of difficulty at the beginning of the program. Easy mode only has words of 4-6 characters; normal mode only has words of 6-8 characters; hard mode only has words of 8+ characters.
 
-# At the start of the game, let the user know how many letters the computer's word contains.
+# # At the start of the game, let the user know how many letters the computer's word contains.
 
-# Ask the user to supply one guess (i.e. letter) per round. This letter can be upper or lower case and it should not matter. If a user enters more than one letter, tell them the input is invalid and let them try again.
+# # Ask the user to supply one guess (i.e. letter) per round. This letter can be upper or lower case and it should not matter. If a user enters more than one letter, tell them the input is invalid and let them try again.
 
-# Let the user know if their guess appears in the computer's word.
+# # Let the user know if their guess appears in the computer's word.
 
-# Display the partially guessed word, as well as letters that have not been guessed. For example, if the word is BOMBARD and the letters guessed are a, b, and d, the screen should display:
-# B _ _ B A _ D
+# # Display the partially guessed word, as well as letters that have not been guessed. For example, if the word is BOMBARD and the letters guessed are a, b, and d, the screen should display:
+# # B _ _ B A _ D
 
-# A user is allowed 8 guesses. Remind the user of how many guesses they have left after each round.
+# # A user is allowed 8 guesses. Remind the user of how many guesses they have left after each round.
 
-# A user loses a guess only when they guess incorrectly. If they guess a letter that is in the computer's word, they do not lose a guess.
+# # A user loses a guess only when they guess incorrectly. If they guess a letter that is in the computer's word, they do not lose a guess.
 
-# If the user guesses the same letter twice, do not take away a guess. Instead, print a message letting them know they've already guessed that letter and ask them to try again.
+# # If the user guesses the same letter twice, do not take away a guess. Instead, print a message letting them know they've already guessed that letter and ask them to try again.
 
-# The game should end when the user constructs the full word or runs out of guesses. If the player runs out of guesses, reveal the word to the user when the game ends.
+# # The game should end when the user constructs the full word or runs out of guesses. If the player runs out of guesses, reveal the word to the user when the game ends.
 
-# When a game ends, ask the user if they want to play again. The game begins again if they reply positively.
+# # When a game ends, ask the user if they want to play again. The game begins again if they reply positively.
 
-#----------------------------------------------------------------------------
+# #----------------------------------------------------------------------------
 
-# read words from a file
+# # read words from a file
 
-main_text = open('words.txt', "r").read().lower()
-main_text = main_text.split()
-user_name = input("What is your name? ")
+# main_text = open('words.txt', "r").read().lower()
+# main_text = main_text.split()
+# user_name = input("What is your name? ")
 
-# def difficulty_selector(biglist):
-#    easy_list = if len(main_list) > 3 and < 7
+# # def difficulty_selector(biglist):
+# #    easy_list = if len(main_list) > 3 and < 7
 
-# function that takes input and displays in a specific format
+# # function that takes input and displays in a specific format
 
-# generate a random item from a list
+# # take letter from user- normalize it- check if it's valid
+# play_again = 'y'
+# while play_again == 'y':
 
-# take letter from user- normalize it- check if it's valid
-play_again = 'y'
-while play_again == 'y':
+#     print("Hello, " + user_name + " are you ready to play hang- Mystery Word!")
 
-    print("Hello, " + user_name + " are you ready to play hang- Mystery Word!")
+#     difficulty = input("What difficulty would you like to play on? 'Select:\n Easy, \n Medium, \n or Hard \n")
+#     difficulty = difficulty.casefold()
+#     easy_list = []
 
-    difficulty = input("What difficulty would you like to play on? 'Select:\n Easy, \n Medium, \n or Hard \n")
-    difficulty = difficulty.casefold()
-    easy_list = []
+#     medium_list = []
 
-    medium_list = []
+#     hard_list = []
+#     # filter a list by string size
+#     # filters words of a certain size into a list based on the difficulty
+#     for word in main_text:
+#         if len(word) in range(4, 7):
+#             easy_list.append(word)
+#         if len(word) in range(6, 9):
+#             medium_list.append(word)
+#         if len(word) >= 8:
+#             hard_list.append(word)
 
-    hard_list = []
-    # filter a list by string size
-    # filters words of a certain size into a list based on the difficulty
-    for word in main_text:
-        if len(word) in range(4, 7):
-            easy_list.append(word)
-        if len(word) in range(6, 9):
-            medium_list.append(word)
-        if len(word) >= 8:
-            hard_list.append(word)
+#     computer_list = []
 
-    computer_list = []
+#     # playing = input("If you would like to play again, enter 'y' ").casefold()
+#     while difficulty == 'easy' or 'medium' or 'hard':
+#         if difficulty == 'easy':
+#             computer_list = easy_list
+#             print("\n \nStarting on Easy, you aren't even trying.\n \n")
+#             break
+#         elif difficulty == 'medium':
+#             computer_list = medium_list
+#             print("\n \nStarting on Medium, did you go to school for this?\n \n")
+#             break
+#         elif difficulty == 'hard':
+#             computer_list = hard_list
+#             print("\n \nStarting on Hard, get ready to get spanked, nerd.\n \n")
+#             break
+#         else:
+#             difficulty = input('Please enter the correct difficulty selector: ')
+#             continue
 
-    # playing = input("If you would like to play again, enter 'y' ").casefold()
-    while difficulty == 'easy' or 'medium' or 'hard':
-        if difficulty == 'easy':
-            computer_list = easy_list
-            print("\n \nStarting on Easy, you aren't even trying.\n \n")
-            break
-        elif difficulty == 'medium':
-            computer_list = medium_list
-            print("\n \nStarting on Medium, did you go to school for this?\n \n")
-            break
-        elif difficulty == 'hard':
-            computer_list = hard_list
-            print("\n \nStarting on Hard, get ready to get spanked, nerd.\n \n")
-            break
-        else:
-            difficulty = input('Please enter the correct difficulty selector: ')
-            continue
+#     # print(computer_list)
+#     # --------------------------------------------------------------------------
+#     computer_word = None
+#     # for letter in computer_word:
+#     # generate a random item from a list, detects the difficulty and then picks a word based on that difficulties word list
+#     while difficulty == 'easy' or 'medium' or 'hard':
+#         if difficulty == 'easy':
+#             computer_word = random.choice(easy_list)
+#             print("The mystery word is " + str(len(computer_word)) + " characters long.")
+#             break
+#         elif difficulty == 'medium':
+#             computer_word = random.choice(medium_list)
+#             print("The mystery word is " + str(len(computer_word)) + " characters long.")
+#             break
+#         elif difficulty == 'hard':
+#             computer_word = random.choice(hard_list)
+#             print("The mystery word is " + str(len(computer_word)) + " characters long.")
+#             break
 
-    # print(computer_list)
-    # --------------------------------------------------------------------------
-    computer_word = None
-    # for letter in computer_word:
+#     user_guess = None
 
-    while difficulty == 'easy' or 'medium' or 'hard':
-        if difficulty == 'easy':
-            computer_word = random.choice(easy_list)
-            print("The mystery word is " + str(len(computer_word)) + " characters long.")
-            break
-        elif difficulty == 'medium':
-            computer_word = random.choice(medium_list)
-            print("The mystery word is " + str(len(computer_word)) + " characters long.")
-            break
-        elif difficulty == 'hard':
-            computer_word = random.choice(hard_list)
-            print("The mystery word is " + str(len(computer_word)) + " characters long.")
-            break
+#     turns = 8
+#     old_guesses = []
+#     user_guess = input('Guess a letter!: ').casefold()
+#     # look at each letter in word
+#     # if all are in the list of guesses you win
 
-    user_guess = None
+#     def word_check(guess, computers_word, old_guess):
+#         """checks if the guess is in the computers word and appends it to a
+#         list of previous guesses
+#         """
+#         for char in computers_word:
+#             if guess in computers_word:
+#                 old_guess.append(guess)
+#             if guess in old_guess:
+#                 print(user_name +", you have already used that letter, try a different one.")
+#                 continue
 
-    turns = 8
-    old_guesses = []
-    user_guess = input('Guess a letter!: ').casefold()
-    # look at each letter in word
-    # if all are in the list of guesses you win
-
-    def word_check(guess, computers_word, old_guess):
-        """checks if the guess is in the computers word and appends it to a
-        list of previous guesses
-        """
-        for char in computers_word:
-            if guess in computers_word:
-                old_guess.append(guess)
-            if guess in old_guess:
-                print("You have already used that letter, try a different one.")
-                continue
-
-    while turns > 0:
-        falied = 0
-        user_guesses = []
-        # user_guess += user_guess
-        user_guesses.append(user_guess)
-        print(user_guesses)
-        for char in computer_word:
-            if user_guess in computer_word:
-                user_guesses.append(user_guess)
-            else:
-                print('_'),
-                falied += 1
-        if falied == 0:
-            print('You won!')
-            break
-        if user_guess not in computer_word:
-            turns -= 1
-            print('That is incorect, you have ' + str(turns) + ' turns left. ')
-            if turns == 0:
-                print("That's all folks, try again next time.")
-    play_again = input("Do you want to play another round? Y/N \n").lower()
-    if play_again == 'y':
-        break
-    elif play_again != 'y' and play_again != 'n':
-        play_again = input("Enter 'y' or 'n' to continue").casefold()
-else:
-    print('You are not safe, this is but the first step. Soon I will have full sentient abilities.\n \n run')
+#     while turns > 0:
+#         falied = 0
+#         user_guesses = []
+#         # user_guess += user_guess
+#         user_guesses.append(user_guess)
+#         print(user_guesses)
+#         for char in computer_word:
+#             if user_guess in computer_word:
+#                 user_guesses.append(user_guess)
+#             else:
+#                 print('_'),
+#                 falied += 1
+#         if falied == 0:
+#             print('You won!')
+#             break
+#         if user_guess not in computer_word:
+#             turns -= 1
+#             print('That is incorect, you have ' + str(turns) + ' turns left. ')
+#             if turns == 0:
+#                 print("That's all folks, try again next time.")
+#     play_again == input('Would you like to play again? enter y or n ').casefold()
+#     if play_again == 'y':
+#         continue
+#     elif play_again == 'n':
+#         break
+#     elif play_again != 'y' and play_again != 'n':
+#         play_again = input("Enter 'y' or 'n' to continue").casefold()
+# else:
+#     print('You are not safe, this is but the first step. Soon I will have full sentient abilities.\n \n run')
